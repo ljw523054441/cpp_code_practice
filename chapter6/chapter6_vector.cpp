989827995c49fd1d50d7cf189184f56578caf9a7
JW_Li <ljw523054441@gmail.com>
2020-07-12 22:51:12 +0800

    part3

--- chapter6/chapter6_vector.cpp
+++ chapter6/chapter6_vector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>//sort函数所在的头文件
 
 using namespace std;
 
@@ -14,4 +15,22 @@ int main()
         cout << vec_double[i] << endl;
     }
     
+    //vector的通用遍历方法：使用迭代器iterator
+    //以下是迭代器的基本用房
+    vector<double>::iterator it;//得到迭代器对象，要得到vector<double>类型的这样一个迭代器，需要用到一个域运算符iterator,实际上是一个指针对象！
+    //it.begin()
+    for (it = vec_double.begin(); it != vec_double.end(); ++it)
+    {
+        cout << *it << endl;
+    }
+
+    //排序
+    sort(vec_double.begin(),vec_double.end());
+    //逆序
+    // reverse(vec_double.begin(),vec_double.end());
+    for (it = vec_double.begin(); it != vec_double.end(); ++it)
+    {
+        cout << *it << endl;
+    }
+
 }
