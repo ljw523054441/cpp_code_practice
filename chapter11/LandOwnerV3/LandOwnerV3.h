39c60ad834e078f6f76e77521b1c28c37a5db2f8
JW_Li <ljw523054441@gmail.com>
2020-07-17 16:41:38 +0800

    class

---
+++ chapter11/LandOwnerV3/LandOwnerV3.h
@@ -0,0 +1,20 @@
+#ifndef LANDOWNERV3_H
+#define LANDOWNERV3_H
+class LandOwnerV3
+{
+public:
+	LandOwnerV3();
+	~LandOwnerV3();
+
+private:
+
+};
+
+LandOwnerV3::LandOwnerV3()
+{
+}
+
+LandOwnerV3::~LandOwnerV3()
+{
+}
+#endif // !LANDOWNERV3_H
