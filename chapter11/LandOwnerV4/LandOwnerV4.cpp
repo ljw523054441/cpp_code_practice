1ca8e803b654c9884c0a4061c7241a8c008771a3
JW_Li <ljw523054441@gmail.com>
2020-07-20 13:24:46 +0800

    class

--- chapter11/LandOwnerV4/LandOwnerV4.cpp
+++ chapter11/LandOwnerV4/LandOwnerV4.cpp
@@ -3,19 +3,19 @@
 LandOwnerV4::LandOwnerV4()
 {
     //一般使用构造函数进行成员变量的初始化
-    name = "默认地主";
-    score = 0;
+    _name = "默认地主";
+    _score = 0;
     //将用户手牌数组初始化为0
-    memset(cards,0,sizeof(cards));
+    memset(_cards,0,sizeof(_cards));
     
     cout << "LandOwner4的无参构造函数（默认构造）被调用" << endl;
     cout << "初始化的结果如下：" << endl;
-    cout << "名称：" << name << endl;
-    cout << "积分：" << score <<endl;
+    cout << "名称：" << _name << endl;
+    cout << "积分：" << _score <<endl;
     cout << "手牌：";
-    for(int i = 0;i<sizeof(cards)/sizeof(cards[0]);i++)
+    for(int i = 0;i<sizeof(_cards)/sizeof(_cards[0]);i++)
     {
-        cout << cards[i] << ",";
+        cout << _cards[i] << ",";
     }
     cout << endl;
 }
--- chapter11/LandOwnerV4/LandOwnerV4.h
+++ chapter11/LandOwnerV4/LandOwnerV4.h
@@ -29,4 +29,4 @@ private:
 };
 
 
-#endif LANDOWNERV4_H
+#endif //LANDOWNERV4_H
